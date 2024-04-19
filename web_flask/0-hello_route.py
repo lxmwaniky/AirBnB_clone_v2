46cfe78585f0d2d9986b13452fcfd342ec052260
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-19 21:08:43 +0300

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """Displays 'Hello HBNB!'"""
-    return "<p>Hello HBNB!</p>"
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
