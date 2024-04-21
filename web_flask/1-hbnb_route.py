d95b28b19be247604e1cd53cfa50abaf545dea85
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-21 17:43:37 +0300

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Flask App
+Listens on 0.0.0.0:5000
+Routes '/' display Hello HBNB
+    '/hbnb' display HBNB
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+if __name___ == '__main__':
+    app.run(host="0.0.0.0")
