d427f97785e2c2546c2150dba4df64134c5c0aed
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-21 16:46:19 +0300

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-Starts Flask Web App
-Listens on 0.0.0.0:5000
+"""Starts Flask web app
+Listening on 0.0.0.0:5000
+Route '/' displays "Hello HBNB!"
 """
 from flask import Flask
 
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_route():
     """Displays 'Hello HBNB!'"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
