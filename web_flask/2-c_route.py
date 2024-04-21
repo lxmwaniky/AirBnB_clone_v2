a82ea5d31e92dd282807c9a1eb85eccc3ce51399
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-21 18:08:21 +0300

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Start Flask App
+Listens on 0.0.0.0:5000
+Routes:
+    '/' - display “Hello HBNB!”
+    '/hbnb' - display “HBNB”
+    '/c/<text>' - display “C ” followed by the value of the text
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """Displays C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
